84c21464889212c2f0807d74f2d3608ca0c71149
Matthew Hokanson <m@h0ke.com>
2012-08-20 23:19:10 -0700

    adding ability to have images link to instagram or to large image.

--- example/js/jquery-embedagram.pack.js
+++ example/js/jquery-embedagram.pack.js
@@ -1,8 +1,8 @@
 /*
  * embedagram - embed your instagram photos
- *   http://embedagram.com/plugin
+ *   http://embedagram.com/plugin.php
  *
- * Copyright (c) 2011 Matthew Hokanon (http://h0ke.com)
+ * Copyright (c) 2011 Matthew Hokanson (http://h0ke.com)
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  */
-(function($){$.fn.extend({embedagram:function(options){var defaults={instagram_id:-999,thumb_width:100,wrap_tag:'li',limit:20,success:function(){return true},};var options=$.extend(defaults,options);return this.each(function(){var o=options;var obj=$(this);var jsonp_url="http://embedagram.com/e/plugin/"+o.instagram_id+"/?callback=?";jsonp_url+="&thumb_width="+o.thumb_width+"&wrap_tag="+o.wrap_tag;jsonp_url+="&limit="+o.limit;$.getJSON(jsonp_url,function(data){obj.html(data.html);o.success.apply(obj)});return obj})}})})(jQuery);
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(0($){$.q.h({7:0(d){1 e={8:-v,4:u,2:\'n\',3:\'m\',5:l,f:0(){6 j},};1 d=$.h(e,d);6 g.k(0(){1 o=d;1 b=$(g);1 c="p://7.i/e/r/"+o.8+"/?s=?";c+="&4="+o.4+"&2="+o.2;c+="&5="+o.5+"&3="+o.3;$.t(c,0(a){b.9(a.9);o.f.w(b)});6 b})}})})(x);',34,34,'function|var|wrap_tag|link_type|thumb_width|limit|return|embedagram|instagram_id|html||||||success|this|extend|com|true|each|20|web|li||http|fn|plugin|callback|getJSON|100|999|apply|jQuery'.split('|'),0,{}))
