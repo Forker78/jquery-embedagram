8ea3662c1477a83ecfbdf9e6469eff76d2216485
Hoke <m@h0ke.com>
2014-12-06 10:01:35 -0800

    update the example code w/ new pack file. also update the html

--- example/js/jquery-embedagram.pack.js
+++ example/js/jquery-embedagram.pack.js
@@ -2,7 +2,7 @@
  * embedagram - embed your instagram photos
  *   http://embedagram.com/plugin.php
  *
- * Copyright (c) 2011 Matthew Hokanson (http://h0ke.com)
+ * Copyright (c) 2011-2014 Matthew Hokanson (http://h0ke.com)
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  */
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(0($){$.q.h({7:0(d){1 e={8:-v,4:u,2:\'n\',3:\'m\',5:l,f:0(){6 j},};1 d=$.h(e,d);6 g.k(0(){1 o=d;1 b=$(g);1 c="p://7.i/e/r/"+o.8+"/?s=?";c+="&4="+o.4+"&2="+o.2;c+="&5="+o.5+"&3="+o.3;$.t(c,0(a){b.9(a.9);o.f.w(b)});6 b})}})})(x);',34,34,'function|var|wrap_tag|link_type|thumb_width|limit|return|embedagram|instagram_id|html||||||success|this|extend|com|true|each|20|web|li||http|fn|plugin|callback|getJSON|100|999|apply|jQuery'.split('|'),0,{}))
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(0($){$.n.9({g:0(d){1 e={f:-t,3:m,4:\'u\',2:\'l\',5:w,7:0(){6 i},};d=$.9(e,d);6 8.k(0(){1 o=d;1 b=$(8);1 c="//g.p/e/q/"+o.f+"/?r=?";c+="&3="+o.3+"&4="+o.4;c+="&5="+o.5+"&2="+o.2;$.s(c,0(a){b.h(a.h);o.7.v(b)});6 b})}})})(j);',33,33,'function|var|link_type|thumb_width|wrap_tag|limit|return|success|this|extend||||||instagram_id|embedagram|html|true|jQuery|each|web|100|fn||com|plugin|callback|getJSON|999|li|apply|20'.split('|'),0,{}))
